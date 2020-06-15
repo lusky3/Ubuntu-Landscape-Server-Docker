76d2a77b2799d96b33fcdeb4bf00205c447d2f6f
ChaosZero112 <ChaosZero112@users.noreply.github.com>
2020-06-14 20:24:29 -0400

    Update domain.sh with a bit of logic checking.

--- scripts/domain.sh
+++ scripts/domain.sh
@@ -3,45 +3,64 @@
 # Description: Setup achme.sh and obtain Let'sEncrypt certificate
 #
 if [[ -z ${DOMAIN} ]]; then
-    echo "Domain ENV is empty. Kill script."
+    echo "domain.sh: Domain ENV is empty. Kill script."
     exit 1
 fi
-# Clone acme.sh repo from github
-git clone https://github.com/acmesh-official/acme.sh.git /tmp/achme.sh && \
-    cd /tmp/acme.sh && \
-    # run the acme.sh installer
-    ./acme.sh --install && \
-    # Add a link to acme.sh in bin for ease of access
-    ln -s /.acme.sh/acme.sh /usr/bin/acme.sh && \
-    # Certificates will be placed in /etc/ssl/certs
-    mkdir -p /etc/ssl/certs && \
-    mkdir -p /etc/ssl/private
-# Cloudflare
-if [[ -n "${CF_Token}" && -n "${CF_Account_ID}" ]] || [[ -n "${CF_Key}" && -n "${CF_Email}" ]; then
-    DNS_METHOD="--dns dns_cf"
-# AWS Route53
-elif [ -n "${AWS_ACCESS_KEY_ID}" && -n "${AWS_SECRET_ACCESS_KEY}" ]; then
-    DNS_METHOD="--dns dns_aws"
-# FreeDNS
-elif [ -n "${FREEDNS_User}" && -n "${FREEDNS_Password}" ]; then
-    DNS_METHOD="--dns dns_freedns"
-# Fallback to Apache
-else
-    DNS_METHOD="--apache"
-fi
-# Check if ECDSA is wanted (Default = true)
-if [[ "${ECDSA}" -eq "true" ]]; then
-    ECDSA=" --ecc ec-256"
-else
-    ECDSA=""
-fi
-# Request the sertificate from Let'sEncrypt
-acme.sh --issue $DNS_METHOD -d $DOMAIN$ECDSA
-# Install the certificate
-acme.sh --install-cert -d $DOMAIN${ECDSA:0:7} \
---cert-file      /etc/ssl/certs/landscape_server.pem  \
---key-file       /etc/ssl/private/landscape_server.key  \
---fullchain-file /etc/ssl/certs/landscape_server_ca.crt \
---reloadcmd     "service apache2 force-reload"
 
+# Check that acme doesn't already exist or update it if it does
+if [[ /root/.acme ]]; then
+    acme.sh --upgrade
+else
+    # Clone acme.sh repo from github
+    git clone https://github.com/acmesh-official/acme.sh.git /tmp/achme.sh && \
+        cd /tmp/acme.sh && \
+        # run the acme.sh installer
+        ./acme.sh --install && \
+        # Add a link to acme.sh in bin for ease of access
+        ln -s /.acme.sh/acme.sh /usr/bin/acme.sh && \
+        # Certificates will be placed in /etc/ssl/certs
+        mkdir -p /etc/ssl/certs && \
+        mkdir -p /etc/ssl/private
+fi
+
+# We don't want to waste our time if the certificates already exist
+if [[ -d /root/.acme/$DOMAIN ]] || [[ -d /root/.acme/$DOMAIN_ecc ]]; then
+    echo "domain.sh: Certificate for $DOMAIN appears to already exist, so we will only try to install it."
+    mkdir -p /etc/ssl/certs && \
+        mkdir -p /etc/ssl/private
+    acme.sh --install-cert -d $DOMAIN${ECDSA:0:7} \
+        --cert-file      /etc/ssl/certs/landscape_server.pem  \
+        --key-file       /etc/ssl/private/landscape_server.key  \
+        --fullchain-file /etc/ssl/certs/landscape_server_ca.crt \
+        --reloadcmd     "service apache2 force-reload"
+    exit 0
+else
+    # Cloudflare
+    if [[ -n "${CF_Token}" && -n "${CF_Account_ID}" ]] || [[ -n "${CF_Key}" && -n "${CF_Email}" ]; then
+        DNS_METHOD="--dns dns_cf"
+    # AWS Route53
+    elif [ -n "${AWS_ACCESS_KEY_ID}" && -n "${AWS_SECRET_ACCESS_KEY}" ]; then
+        DNS_METHOD="--dns dns_aws"
+    # FreeDNS
+    elif [ -n "${FREEDNS_User}" && -n "${FREEDNS_Password}" ]; then
+        DNS_METHOD="--dns dns_freedns"
+    # Fallback to Apache
+    else
+        DNS_METHOD="--apache"
+    fi
+    # Check if ECDSA is wanted (Default = true)
+    if [[ "${ECDSA}" -eq "true" ]]; then
+        ECDSA=" --ecc ec-256"
+    else
+        ECDSA=""
+    fi
+    # Request the sertificate from Let'sEncrypt
+    acme.sh --issue $DNS_METHOD -d $DOMAIN$ECDSA
+    # Install the certificate
+    acme.sh --install-cert -d $DOMAIN${ECDSA:0:7} \
+    --cert-file      /etc/ssl/certs/landscape_server.pem  \
+    --key-file       /etc/ssl/private/landscape_server.key  \
+    --fullchain-file /etc/ssl/certs/landscape_server_ca.crt \
+    --reloadcmd     "service apache2 force-reload"
+fi
 exit 0
